a1c86dc9e0b095b8f079b94d73d53fc6ef90ea45
黄鑫远 <huangxinyuan@dushu365.com>
2024-10-14 17:03:24 +0800

    feat:代码结构重构

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
@@ -11,16 +13,21 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+// 介绍：命名返回值（prefix string），形参后面的括号可以用于创建一个名为prefix的局部变量，作用域在函数内
+// 另外：在 Go 中，公共函数以大写字母开始，私有函数以小写字母开头。
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Spanish":
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return prefix
 }
 
 func main() {
